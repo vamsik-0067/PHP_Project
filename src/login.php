79180959ce7c4658da79b2be52a123c7571273b7
vamsik-0067 <kv0555989@gmail.com>
2026-08-15 02:47:59 +0530

    Update login.php

--- src/login.php
+++ src/login.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en" xml:lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1">
